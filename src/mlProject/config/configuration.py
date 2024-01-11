e0f5bf237acdcc9f6b39b4e29520182b2c94d209
Anup Kumar Sahu <anup-kumar.sahu@hpe.com>
2024-01-11 08:35:25 +0530

    Model evaluation stage completed

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,5 +1,5 @@
 from mlProject.constants import *
-from mlProject.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig, ModelTrainerConfig
+from mlProject.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig, ModelEvaluationConfig, ModelTrainerConfig
 from mlProject.utils.yaml_operations import create_directories, read_yaml
 from mlProject.logging import logger
 
@@ -94,3 +94,22 @@ class ConfigurationManager:
         logger.debug("Exiting get_model_trainer_config method of ConfigurationManager class")
 
         return model_trainer_config
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.param.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name
+           
+        )
+
+        return model_evaluation_config
